456f456c63e817a82ab82fda443c97b13a12bb17
Rajesh Salian <rajeshsalian@rocketmortgage.com>
2023-02-06 19:13:19 -0500

    Improvising Battle game

--- src/script.js
+++ src/script.js
@@ -2,26 +2,18 @@
 
 const randomDamage = () => +`${Math.floor(Math.random() * 10) + 1}`;
 
-const chooseOption = (opt1, opt2) => {
-    const randNum = Math.round(Math.random());
-    return randNum === 0 ? opt1 : opt2;
-}
+const chooseOption = (opt1, opt2) => Math.round(Math.random()) === 0 ? opt1 : opt2;
 
 const attackPlayer = function(health) {
     return health - randomDamage();
 }
 
-const logHealth = (player, health) => {
-    console.log(`${player} health: ${health}`);
-}
+const logHealth = (player, health) => console.log(`${player} health: ${health}`);
 
-const logDeath = (winner, loser) => {
-    console.log(`${winner} defeated ${loser}`);
-}
 
-const isDead = (health) => {
-        return health <= 0;
-}    
+const logDeath = (winner, loser) => console.log(`${winner} defeated ${loser}`);
+
+const isDead = (health) => health <= 0;
 
 function fight (player1, player2, player1Health, player2Health) {
     
